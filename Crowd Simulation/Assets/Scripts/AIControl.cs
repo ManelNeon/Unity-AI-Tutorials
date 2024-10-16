27c8ab23d9c5bec399a05c4e5f3e71cc2c39b15f
Manuel Gonçalves <neonextra0@gmail.com>
2024-10-16 19:42:19 +0100

    Fleeing

--- Crowd Simulation/Assets/Scripts/AIControl.cs
+++ Crowd Simulation/Assets/Scripts/AIControl.cs
@@ -9,6 +9,11 @@ public class AIControl : MonoBehaviour {
     NavMeshAgent agent;
     Animator anim;
 
+    float sm;
+
+    float detectionRadius = 5;
+
+    float fleeRadius = 10;
 
     void Start() {
 
@@ -22,21 +27,58 @@ public class AIControl : MonoBehaviour {
 
         agent.SetDestination(goalLocations[i].transform.position);
 
-        anim.SetTrigger("isWalking");
-
         anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
 
-        float sm = Random.Range(0.5f, 2.0f);
+        ResetAgent();
+    }
+
+    void ResetAgent()
+    {
+        agent.speed = 2;
+
+        sm = Random.Range(0.1f, 1.0f);
 
         anim.SetFloat("speedMult", sm);
 
         agent.speed *= sm;
+
+        anim.SetTrigger("isWalking");
+
+        agent.angularSpeed = 120;
+
+        agent.ResetPath();
     }
 
+    public void DetectNewObstacle(Vector3 position)
+    {
+        if (Vector3.Distance(position, this.transform.position) < detectionRadius)
+        {
+            Vector3 fleeDirection = (this.transform.position - position).normalized;
+
+            Vector3 newGoal = this.transform.position + fleeDirection * fleeRadius;
+
+            NavMeshPath path = new NavMeshPath();
+
+            agent.CalculatePath(newGoal, path);
+
+            if(path.status != NavMeshPathStatus.PathInvalid)
+            {
+                agent.SetDestination(path.corners[path.corners.Length - 1]);
+
+                anim.SetTrigger("isRunning");
+
+                agent.speed = 10;
+
+                agent.angularSpeed = 500;
+            }
+        }
+    }
 
     void Update() {
         if(agent.remainingDistance < 1)
         {
+            ResetAgent();
+
             int i = Random.Range(0, goalLocations.Length);
 
             agent.SetDestination(goalLocations[i].transform.position);
